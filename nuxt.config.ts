061de974e1e0c150d51bd09f194feadc5daa2479
LOUSANPANG <1271255653@qq.com>
2022-09-01 18:37:59 +0800

    build(deps): :wrench: build router baser

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  router: {
+    base: './',
+  },
+});
