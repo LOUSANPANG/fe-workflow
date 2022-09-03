45f17e8c51fe8a3f7acfada8572664b9ce643280
LOUSANPANG <1271255653@qq.com>
2022-09-03 11:44:29 +0800

    chore(workflows): :wrench: app baseURL NODE_ENV

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,6 @@ import { defineNuxtConfig } from 'nuxt';
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   app: {
-    baseURL: './',
-  },
-
-  router: {
-    base: '/fe-workflow',
+    baseURL: process.env.NODE_ENV === 'production' ? './' : '',
   },
 });
