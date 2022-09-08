d8954814c6eb0d60e5c21c481abffaef7543956b
LOUSANPANG <1271255653@qq.com>
2022-09-08 18:09:27 +0800

    feat(components): add iconfont

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,21 @@
 import { defineNuxtConfig } from 'nuxt'
 
+const isDev = process.env.npm_lifecycle_event === 'dev'
+const isPro = process.env.NODE_ENV === 'production'
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   app: {
-    baseURL: process.env.NODE_ENV === 'production' ? '/fe-workflow' : '',
+    baseURL: isPro ? '/fe-workflow' : '',
+    head: {
+      link: [{ rel: 'shortcut icon', href: '/favicon.ico' }],
+      script: [isDev && { src: 'https://cdn.tailwindcss.com' }],
+    },
   },
 
   modules: ['@nuxt/content'],
 
-  css: ['~/assets/css/tailwind.css'],
+  css: ['~/assets/css/tailwind.css', '~/assets/css/iconfont.css'],
 
   build: {
     postcss: {
