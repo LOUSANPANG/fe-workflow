95e078c5d83452a71beffe903cc5c7b313a9b7da
LOUSANPANG <1271255653@qq.com>
2022-09-26 14:53:16 +0800

    style(components): docs pages layout

--- components/DocsLayoutLeft.vue
+++ components/DocsLayoutLeft.vue
@@ -3,16 +3,17 @@
     <ul>
       <!-- earlyDev -->
       <li v-for="item in docsLayoutLeftEarlyDev" :key="item.title">
-        <a
-          :href="item.router"
+        <NuxtLink
+          :to="item.router"
           class="inline-flex items-center text-sm leading-6 mb-4 font-medium text-slate-700 hover:text-sky-500 hover:shadow-sm"
+          activeClass="text-sky-500"
         >
           <span
             class="mr-4 p-[2px] !text-xl !leading-4 rounded ring-1 ring-slate-900/5 shadow-md"
             :class="item.icon"
           />
           {{ item.title }}
-        </a>
+        </NuxtLink>
       </li>
 
       <!-- guide -->
@@ -20,10 +21,11 @@
         <h5 class="mb-3 font-medium text-slate-900">{{ item.title }}</h5>
         <ul class="space-y-2 border-l border-slate-100">
           <li v-for="val in item.children" :key="val.title">
-            <a
-              :href="val.router"
-              class="block border-l pl-4 -ml-px border-transparent text-slate-500 hover:border-slate-400 hover:text-slate-900"
-              >{{ val.title }}</a
+            <NuxtLink
+              :to="val.router"
+              class="block border-l pl-4 -ml-px border-slate-200 text-slate-500 hover:border-slate-400 hover:text-slate-900"
+              activeClass="text-sky-500 border-sky-400"
+              >{{ val.title }}</NuxtLink
             >
           </li>
         </ul>
