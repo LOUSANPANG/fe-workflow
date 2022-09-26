040bbeb9cd708178d986393569d8dac81ae1125a
LOUSANPANG <1271255653@qq.com>
2022-09-26 17:29:34 +0800

    ci(other): baseURL env error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,9 @@
 const isDev = process.env.npm_lifecycle_event === 'dev'
-const isPro = process.env.NODE_ENV === 'production'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   app: {
-    baseURL: isPro ? '/fe-workflow' : '',
+    baseURL: isDev ? '' : '/fe-workflow',
     head: {
       link: [{ rel: 'shortcut icon', href: '/fe-workflow/favicon.ico' }],
       script: [isDev && { src: 'https://cdn.tailwindcss.com' }],
