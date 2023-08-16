0f67aecdd16ea580c9f47505c43a1353e4ce0754
LOUSANPANG <1271255653@qq.com>
2023-08-16 17:16:15 +0800

    :construction_worker: ci(workflows): test yml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 const isDev = process.env.npm_lifecycle_event === 'dev'
-const baseURL = isDev ? '' : '/fe-workflow'
+// const baseURL = isDev ? '' : '/fe-workflow'
+const baseURL = '/fe-workflow'
+console.info('process.env.npm_lifecycle_event ===>', isDev)
 
 export default defineNuxtConfig({
   typescript: {
@@ -15,7 +17,8 @@ export default defineNuxtConfig({
     baseURL,
     head: {
       link: [{ rel: 'shortcut icon', href: `${baseURL}/favicon.ico` }],
-      script: isDev ? [{ src: 'https://cdn.tailwindcss.com' }] : [],
+      // script: isDev ? [{ src: 'https://cdn.tailwindcss.com' }] : [],
+      script: [],
     },
   },
 
